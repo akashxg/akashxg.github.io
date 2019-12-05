9f4e031b2927d4513371a80ee82a24a66ee0a0de
Gheewala <gheakash@38f9d35e5a15.ant.amazon.com>
2019-12-05 13:33:04 -0500

    updated script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
     $("#EC2_TEST").click(function() {
       console.log("made it inside EC2");
       //var sendData = $('#EC2_TEST').val();
-      var sendData = "hello Sam";
-       $.ajax({
+      var sendData = { operation: "create", table_name: "Capstone", payload: { key: "Sam", value: "is dumb" } };
+      $.ajax({
           type: 'POST',
           url: 'https://n0coguumod.execute-api.us-east-1.amazonaws.com/test_1',
-          data: sendData,
+          data: JSON.stringify(sendData),
+          crossDomain: true,
           contentType: "application/json",
           dataType: "json",
           success: function (data, status){
