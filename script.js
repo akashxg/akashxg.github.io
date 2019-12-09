a662892166711ae08d257900a6b1002ad5b7bcf2
Gheewala <gheakash@38f9d35e5a15.ant.amazon.com>
2019-12-09 17:48:12 -0500

    update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ $(document).ready(function() {
           contentType: "application/json",
           dataType: "json",
           success: function (data, status){
-            alert("Data: "+ data + "\nStatus: "+ status);
+            alert("Data: "+ data["body"] + "\nStatus: "+ status);
           }
       });
 
